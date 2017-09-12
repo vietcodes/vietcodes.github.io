03ce5f277becd8bfebc962a4cba840bccf2d7fa1
Do Duy <doduycm@gmail.com>
2017-09-12 09:21:50 +0700

    Tue Sep 12 09:21:50 +07 2017

--- code/46/main.cpp
+++ code/46/main.cpp
@@ -5,6 +5,10 @@
 using namespace std;
 #define LL long long
 
+// Tính diện tích hình với `n` là số đỉnh và `x`, `y` là tọa độ các đỉnh.
+//
+// Ta không nhân với 1/2 như công thức ở trên nên
+// kết quả trả về là 2 lần diện tích hình cần tính.
 LL S(int n, const vector<LL> &x, const vector<LL> &y) {
     LL s = 0;
     for (int i=0; i<n; i++) s += (x[i]-x[(i+1)%n])*(y[i]+y[(i+1)%n]);
@@ -13,14 +17,23 @@ LL S(int n, const vector<LL> &x, const vector<LL> &y) {
 
 int f[1000];
 
+// Tìm thửa ruộng lớn nhất với `k` có ý nghĩa như trong đề,
+// `c` là màu được chọn, các thửa ruộng có màu khác sẽ phải
+// chuyển về `c`.
+// `a` là mảng mô tả các thử ruộng với `first` là diện tích
+// thửa ruộng, `second` là màu của thửa ruộng.
 LL process(const vector< pair<LL, int> > &a, int k, int c) {
     int m = a.size();
 
+    // Mảng f có ý nghĩa:
+    // f[i] là số thửa ruộng cần đổi màu để chuyển 
+    // tất cả các thử ruộng từ 0 đến i về màu c.
     f[0] = a[0].second != c;
     for (int i=1; i<m; i++) {
         f[i] = f[i-1] + (a[i].second != c);
     }
 
+    // duyệt dùng 2 con trỏ l và r.
     LL sum = 0, res = 0;
     for (int r=0, l=0; r<m; r++) {
         sum += a[r].first;
@@ -35,6 +48,7 @@ LL process(const vector< pair<LL, int> > &a, int k, int c) {
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(0);
+
     int m, k; cin >> m >> k;
     vector< pair<LL, int> > a(m);
     for (int i=0; i<m; i++) {
@@ -54,6 +68,9 @@ int main() {
         res = max(res, process(a, k, c));
     }
 
+    // Vì kết quả tính được là 2 lần diện tích
+    // nên ta thực hiện chia 2 như bên dưới để
+    // tránh sử dụng số thực.
     if (res%2 == 1) cout << res/2 << ".5";
     else cout << res/2 << ".0";
 
