4fa0aa4831468e2d40e38a68d92ba701e0bf6ed0
Do Duy <doduycm@gmail.com>
2017-09-22 07:51:02 +0700

    Fri Sep 22 07:51:02 +07 2017

---
+++ code/81/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(0);
+
+    int n; cin >> n;
+    vector<int> a(n);
+    for (int &x: a) cin >> x;
+
+    vector<int> f(n);
+    f[0] = 1;
+    int result = 1;
+    for (int i=1; i<n; i++) {
+        f[i] = 0;
+        for (int j=i-1; j>=0; j--) if (a[i] > a[j]) {
+            f[i] = max(f[i], f[j]);
+        }
+        f[i] += 1;
+        result = max(result, f[i]);
+    }
+
+    cout << result;
+    return 0;
+}
