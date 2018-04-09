f8b0b28ed125cab53d19908312095f71b37261e3
Do Duy <juchiast@gmail.com>
2018-04-09 19:32:36 +0700

    Mon Apr  9 19:32:36 +07 2018

--- js/all.js
+++ js/all.js
@@ -18,19 +18,19 @@ function loadComments() {
     }
 }
 
-var owa_baseUrl = 'https://nilotic-misleads.000webhostapp.com/owa/1.6.0/';
-var owa_cmds = owa_cmds || [];
-owa_cmds.push(['setSiteId', 'd0a9cbff99c9a71664103fda526c3244']);
-owa_cmds.push(['trackPageView']);
-owa_cmds.push(['trackClicks']);
-owa_cmds.push(['trackDomStream']);
+//var owa_baseUrl = 'https://nilotic-misleads.000webhostapp.com/owa/1.6.0/';
+//var owa_cmds = owa_cmds || [];
+//owa_cmds.push(['setSiteId', 'd0a9cbff99c9a71664103fda526c3244']);
+//owa_cmds.push(['trackPageView']);
+//owa_cmds.push(['trackClicks']);
+//owa_cmds.push(['trackDomStream']);
 
 (function() {
     if (window.location.hostname == "vietcodes.github.io") {
         loadComments();
-        var _owa = document.createElement('script'); _owa.type = 'text/javascript'; _owa.async = true;
-        owa_baseUrl = ('https:' == document.location.protocol ? window.owa_baseSecUrl || owa_baseUrl.replace(/http:/, 'https:') : owa_baseUrl );
-        _owa.src = owa_baseUrl + 'modules/base/js/min.js';
-        var _owa_s = document.getElementsByTagName('script')[0]; _owa_s.parentNode.insertBefore(_owa, _owa_s);
+        //var _owa = document.createElement('script'); _owa.type = 'text/javascript'; _owa.async = true;
+        //owa_baseUrl = ('https:' == document.location.protocol ? window.owa_baseSecUrl || owa_baseUrl.replace(/http:/, 'https:') : owa_baseUrl );
+        //_owa.src = owa_baseUrl + 'modules/base/js/min.js';
+        //var _owa_s = document.getElementsByTagName('script')[0]; _owa_s.parentNode.insertBefore(_owa, _owa_s);
     }
 })();
